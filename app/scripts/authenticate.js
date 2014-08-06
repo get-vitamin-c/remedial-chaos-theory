cb09d219f65c0e39d02cc172443c5a6e651bb799
Jordan Burke <me@jordankburke.com>
2014-08-06 17:36:14 -0400

    Removed auth for now

--- app/scripts/authenticate.js
+++ app/scripts/authenticate.js
@@ -4,8 +4,8 @@ function authenticate()
 {
 	var accessToken = sessionStorage.accessToken;
 
-	if (accessToken == undefined)
-	{
-		document.location.assign("/#/login");
-	}
+	// if (accessToken == undefined)
+	// {
+	// 	document.location.assign("/#/login");
+	// }
 }
